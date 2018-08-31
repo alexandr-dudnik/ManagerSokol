f4ed67feb9de07b3dfc6db8d694aa976f79b8fd6
Alexandr Dudnik <dudnik2104@gmail.com>
2018-08-31 07:33:46 +0300

    Goods sync

--- app/src/main/java/com/sokolua/manager/mvp/models/OrderModel.java
+++ app/src/main/java/com/sokolua/manager/mvp/models/OrderModel.java
@@ -9,6 +9,8 @@ import java.util.Date;
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class OrderModel extends AbstractModel {
     public void setDeliveryDate(OrderRealm currentOrder, Date mDate) {
@@ -50,6 +52,10 @@ public class OrderModel extends AbstractModel {
     }
 
     public void updateOrderFromRemote(String orderId) {
-        mDataManager.updateOrderFromRemote(orderId);
+        Observable.just(orderId)
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .doOnNext(id -> mDataManager.updateOrderFromRemote(orderId))
+                .subscribe();
     }
 }
